ed4e0f9741673d30542244919cb6d7544ddab039
ainame <s.namai.09@gmail.com>
2022-12-07 11:35:26 +0000

    Initial Commit

---
+++ BitriseTutorials/BitriseTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseTutorialsApp.swift
+//  BitriseTutorials
+//
+//  Created by Satoshi Namai on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BitriseTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
